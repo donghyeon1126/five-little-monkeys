79219d021fc9a958f28f9fb86be9aea2a6aab325
donghyeon1126 <handonghyeon1126@gmail.com>
2022-01-26 20:46:05 +0900

    add C language

---
+++ C five little monkeys.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	int monkeys = 5;
+	char num_array[6][7] = { "one", "two", "three", "four", "five" };
+	while (monkeys > 0)
+	{
+		if (monkeys == 1)
+		{
+			printf("%s little monkey jumping on the bed\n", num_array[monkeys - 1]);
+		}
+		else
+		{
+			printf("%s little monkeys jumping on the bed\n", num_array[monkeys - 1]);
+		}
+		printf("One fell down and bumped his head.\n");
+		printf("Mama called the doctor and the doctor said.\n\"No more monkeys jumping on the bed!\"\n");
+		monkeys = monkeys - 1;
+	}
+	printf("No little monkeys jumping on the bed.\nNone fell off and bumped his head.\nMama called the doctor and the doctor said.\n\"Put those monkeys right to bed!\"");
+	return 0;
+}
